f07b2cfa9c874ac031715d445539607279e8195e
Rafael <rafaelv.boscato@hotmail.com>
2025-07-15 20:09:20 -0300

    Link agenda to user by retrieving user ID from localStorage

--- src/services/createAgenda.js
+++ src/services/createAgenda.js
@@ -2,6 +2,11 @@
 import { authFetch } from "./authFetch";
 
 export async function createAgenda(agendaData) {
+
+  const userId = localStorage.getItem("user_id");
+
+  agendaData.user_id = userId; // Ensure the agenda is linked to the user
+
   return authFetch("/schedule", {
     method: "POST",
     body: agendaData,
