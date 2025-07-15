b095ff607b536d9f0ceacd3474e514b6268dc52a
Rafael <rafaelv.boscato@hotmail.com>
2025-07-15 19:53:12 -0300

    Fix localStorage key for user ID in getAllAgendas function

--- src/services/getAllAgendas.js
+++ src/services/getAllAgendas.js
@@ -2,6 +2,6 @@
 import { authFetch } from "./authFetch";
 
 export async function getAllAgendas() {
-  const userId = localStorage.getItem("userId");
+  const userId = localStorage.getItem("user_id");
   return authFetch(`/schedule/${userId}`);
 }
